77d109708a120ad4c1ba1d0bbb5e56f5d8551603
justdprroz <dmitri.proskuriakov@gmail.com>
2021-02-15 19:10:27 +0300

    Changed build options

--- build.sh
+++ build.sh
@@ -4,11 +4,8 @@ cd tmp
 if [ $# -eq 0 ]; then
     cmake -DCMAKE_BUILD_TYPE=Release ..
 else
-    if [ "$1" == "0" ]; then
+    if [ "$1" == "Debug" ] || [ "$1" == "D" ] || [ "$1" == "debug" ] || [ "$1" == "d" ]; then
         cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-    fi
-    if [ "$1" == "1" ]; then
-        cmake -DCMAKE_BUILD_TYPE=Release ..
     fi
 fi
 make
